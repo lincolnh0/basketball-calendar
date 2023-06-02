178ec114837a050b156150bcdb2733c600fa45f3
lincolnh0 <me@lincolnh0.com>
2023-06-02 14:55:07 +0100

    Update with 3 images for better chance at getting jersey number correct

--- src/views/TodayView.vue
+++ src/views/TodayView.vue
@@ -3,7 +3,9 @@
   <div class="container">
     <h1 class="player-title">{{ playerName }} - {{ teamName }}</h1>
     <p class="date">Today's date is the</p>
-    <img v-if="imageUrl !== ''" class="player-image" :src="imageUrl" alt="{{playerName}}"/>
+    <div class="image-slide-show">
+      <img class="player-image" v-for="( imageUrl, index ) in this.imageUrls" v-bind:key="index" :src="imageUrl" :alt="playerName"/>
+    </div>
     <p class="date-second">of {{ month }}, {{ year }}</p>
   </div>
 </template>
@@ -16,7 +18,7 @@ export default {
     return {
       playerName: "",
       teamName: "",
-      imageUrl: "",
+      imageUrls: [],
       month: new Date().toLocaleString("en-US", {month: "long"}),
       year: new Date().getFullYear(),
     }
@@ -37,18 +39,13 @@ export default {
 
     const imageResponse = await axios.get(`.netlify/functions/get-images?query=${playerList[randomIndex].innerText} ${teamList[randomIndex].innerText}`)
     const imageResults = imageResponse.data.content["images_results"]
-    let chosenResult;
-    imageResults.every(element => {
-      if (element.source === "NBA.com") {
-        return true;
+    let chosenResult = [];
+    imageResults.forEach(element => {
+      if (element.source !== "NBA.com") {
+        chosenResult.push(element.original);
       }
-      if (Math.random() > 0.5) {
-        return true;
-      }
-      chosenResult = element.original;
     })
-    this.imageUrl = chosenResult;
-    console.log(chosenResult)
+    this.imageUrls = chosenResult.slice(0, 3);
   }
 }
 
@@ -66,9 +63,23 @@ export default {
   font-size: xx-large;
 }
 
+.image-slide-show {
+  display: flex;
+  gap: 10px;
+}
+
+@media only screen and (max-width: 900px) {
+  .image-slide-show {
+    flex-direction: column;
+  }
+}
+
 .player-image {
-  max-width: 100%;
-  max-height: 650px;
+  max-width: 300px;
+  max-height: 400px;
+  height: auto;
+  width: auto;
+  object-fit: cover;
 }
 
 
